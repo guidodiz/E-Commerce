36d607b00ec03db328c165e3c8f7e98782750390
Guido Diz <guidoangeldiz@gmail.com>
2024-11-13 22:18:55 -0300

    creacion de la estructura templates y base.html

--- E-Commerce/TiendaOnline/web/views.py
+++ E-Commerce/TiendaOnline/web/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hola')
+    context={
+        'nombre': 'Guido'
+    }
+    return render(request, 'web/index.html', context)
