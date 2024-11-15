476291d55fe4e0753bba4bffe63a17076cd7ce6b
Guido Diz <guidoangeldiz@gmail.com>
2024-11-14 21:10:28 -0300

    primeros pasos de css

--- E-Commerce/TiendaOnline/web/views.py
+++ E-Commerce/TiendaOnline/web/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context={
-        'nombre': 'Guido'
-    }
-    return render(request, 'web/index.html', context)
+    return render(request, 'web/index.html')
