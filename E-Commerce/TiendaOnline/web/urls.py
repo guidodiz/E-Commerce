86b6798a93fa6535e154b868a0bc0c8301c1aa5a
Guido Diz <guidoangeldiz@gmail.com>
2024-12-02 14:38:27 -0300

    imagenes insertadas en el proyecto

--- E-Commerce/TiendaOnline/web/urls.py
+++ E-Commerce/TiendaOnline/web/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('productos/', views.index, name='productos'),
+    path('contacto/', views.index, name='contacto'),
 ]
--- E-Commerce/TiendaOnline/web/views.py
+++ E-Commerce/TiendaOnline/web/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'web/index.html')
+
+def productos(request):
+    return render(request, 'web/productos.html')
+
+def contacto(request):
+    return render(request, 'web/contacto.html')
