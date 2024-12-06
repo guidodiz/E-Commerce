143e651e2580174e75e81ffb4b99819412ac58c4
Guido Diz <guidoangeldiz@gmail.com>
2024-12-06 15:52:39 -0300

    primeras creaciones de productos y conexiones con bases de datos

--- E-Commerce/TiendaOnline/TiendaOnline/settings.py
+++ E-Commerce/TiendaOnline/TiendaOnline/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": {
+		"ENGINE": "django.db.backends.postgresql",
+		"NAME": "Tienda Online",
+		"USER": "postgres",
+		"PASSWORD": "riverteamo",
+		"HOST": "127.0.0.1",
+		"PORT": "5432",
+	}
 }
 
 
--- E-Commerce/TiendaOnline/web/models.py
+++ E-Commerce/TiendaOnline/web/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    precio_anterior = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio anterior')
+    precio_nuevo = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio nuevo')
