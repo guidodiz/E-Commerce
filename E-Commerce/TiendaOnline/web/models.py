ed27576b8037b15329ddbb7a88169d389d6f7e5f
Guido Diz <guidoangeldiz@gmail.com>
2024-12-09 16:20:47 -0300

    estamos finalizando con la plantilla de vista_producto. Falta poder cambiar de foto principal y mejorar el form para que quede con models.form

--- E-Commerce/TiendaOnline/web/models.py
+++ E-Commerce/TiendaOnline/web/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 class Productos(models.Model):
     nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    descripcion = models.CharField(max_length=110, verbose_name='Descripción', blank=True)
+    talle1 = models.CharField(max_length=15, verbose_name='Talle 1', blank=True)
+    talle2 = models.CharField(max_length=15, verbose_name='Talle 2', blank=True)
+    talle3 = models.CharField(max_length=15, verbose_name='Talle 3', blank=True)
+    talle4 = models.CharField(max_length=15, verbose_name='Talle 4', blank=True)
+    talle5 = models.CharField(max_length=15, verbose_name='Talle 5', blank=True)
     precio_original = models.CharField(max_length=15, verbose_name='Precio original', blank=True)
     precio_anterior = models.CharField(max_length=15, verbose_name='Precio anterior', blank=True)
     precio_nuevo = models.CharField(max_length=15, verbose_name='Precio nuevo', blank=True)
     tiene_descuento = models.BooleanField(default=False, verbose_name='Tiene descuento?')
     imagen = models.ImageField(upload_to='productos/', verbose_name='Imagen')
+    imagen2 = models.ImageField(upload_to='productos/', verbose_name='Imagen 2', blank=True)
+    imagen3 = models.ImageField(upload_to='productos/', verbose_name='Imagen 3', blank=True)
+    imagen4 = models.ImageField(upload_to='productos/', verbose_name='Imagen 4', blank=True)
 
     def __str__(self):
         return f'{self.nombre}'
